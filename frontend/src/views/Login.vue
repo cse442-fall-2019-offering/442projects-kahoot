68cded9222545fbac01ba6f4fcab279eaab90327
shreyavp <svpshreya@gmail.com>
2019-09-22 22:58:16 -0400

    login page

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+
+
+    <div class="content mx-auto">
+        <h2>{{title}}</h2>
+            <div class="form-group">
+                <label for="username">Username </label>
+                <input type="text" />
+            </div>
+            <div class="form-group">
+                <label htmlFor="password">Password </label>
+                <input type="text"/>
+            </div>
+            <div class="form-group">
+                <button class="btn  btn-success">Login</button>
+            </div>
+    </div>
+</template>
+
+
+<script>
+export default{
+    name: 'Login',
+    data(){
+        return{
+            title: 'Login'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+.content {
+  margin-top: 100px;
+  padding-top: 105px;
+}
+
+
+</style>
