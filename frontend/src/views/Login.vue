0050934413a50b2b2d9c1ba4f7cc7aa4635f67a9
shanegri <shgriffin16@gmail.com>
2019-10-14 01:03:47 -0400

    Init Google auth

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,19 +1,21 @@
 <template>
-
-
     <div class="content mx-auto">
-        <h2>{{title}}</h2>
+        <!-- <h2>{{title}}</h2>
             <div class="form-group">
-                <label for="username">Username </label>
-                <input type="text" />
-            </div>
-            <div class="form-group">
-                <label htmlFor="password">Password </label>
-                <input type="text"/>
-            </div>
-            <div class="form-group">
-                <button class="btn  btn-success">Login</button>
-            </div>
+            <label for="username">Username </label>
+            <input type="text" />
+        </div>
+        <div class="form-group">
+            <label htmlFor="password">Password </label>
+            <input type="text"/>
+        </div>
+        <div class="form-group">
+            <button class="btn  btn-success">Login</button>
+        </div> -->
+        <button v-on:click="login" :hidden="authenticated" class="btn btn-primary">Sign in with Google</button>
+        <span :hidden="!authenticated">Logged in as {{ email }}</span> <br>
+        <button v-on:click="logout" :hidden="!authenticated" class="btn btn-warning">Log out</button>
+
     </div>
 </template>
 
@@ -23,16 +25,34 @@ import axios from 'axios';
 
 export default{
     name: 'Login',
-    mounted() {
-        axios
-            .get(process.env.VUE_APP_BACKEND + "/schedule")
-            .then(res => {
-                console.log(res);
-            })
+    data() {
+        return {
+            authenticated: false,
+            email: "",
+        }
     },
-    data(){
-        return{
-            title: 'Login'
+    methods: {
+        login: function() {
+            this.$gAuth.signIn()
+            .then(GoogleUser => {
+                console.log('user', GoogleUser)
+                this.email = GoogleUser.getBasicProfile().U3
+                this.isSignIn = this.$gAuth.isAuthorized
+                this.authenticated = true;
+            })
+            .catch(error  => {
+            //on fail do something
+            })
+        },
+        logout: function() {
+            this.$gAuth.signOut()
+            .then(() => {
+                this.authenticated = false;
+            
+            })
+            .catch(error  => {
+            
+            })
         }
     }
 }
