44c7734a225574bd07740e9de8cd8411b1a0d3d1
shanegri <shgriffin16@gmail.com>
2019-10-21 08:15:27 -0400

    Login/Logout

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -12,9 +12,9 @@
         <div class="form-group">
             <button class="btn  btn-success">Login</button>
         </div> -->
-        <button v-on:click="login" :hidden="authenticated" class="btn btn-primary">Sign in with Google</button>
-        <span :hidden="!authenticated">Logged in as {{ email }}</span> <br>
-        <button v-on:click="logout" :hidden="!authenticated" class="btn btn-warning">Log out</button>
+        <button v-on:click="login" :hidden="$store.getters.authenticated" class="btn btn-primary">Sign in with Google</button>
+        <span :hidden="!$store.getters.authenticated">Logged in as {{ $store.getters.email }}</span> <br>
+        <button v-on:click="logout" :hidden="!$store.getters.authenticated" class="btn btn-warning">Log out</button>
 
     </div>
 </template>
@@ -25,20 +25,14 @@ import axios from 'axios';
 
 export default{
     name: 'Login',
-    data() {
-        return {
-            authenticated: false,
-            email: "",
-        }
-    },
     methods: {
         login: function() {
             this.$gAuth.signIn()
             .then(GoogleUser => {
                 console.log('user', GoogleUser)
-                this.email = GoogleUser.getBasicProfile().U3
-                this.isSignIn = this.$gAuth.isAuthorized
-                this.authenticated = true;
+                this.$store.commit('login', GoogleUser.getBasicProfile().U3);
+                this.$router.push("create");
+                // this.isSignIn = this.$gAuth.isAuthorized
             })
             .catch(error  => {
             //on fail do something
@@ -47,8 +41,7 @@ export default{
         logout: function() {
             this.$gAuth.signOut()
             .then(() => {
-                this.authenticated = false;
-            
+                this.$store.commit('logout');
             })
             .catch(error  => {
             
