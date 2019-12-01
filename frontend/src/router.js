db69a0bb1a60742fa47da58331721242af4e84d6
Gitanjali Nandi <gnandi@buffalo.edu>
2019-12-01 16:59:44 -0500

    Added a help page for valid scheduling rules

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Login from './views/Login.vue';
+import Help from './views/Help.vue';
 import Schedules from './views/Schedules.vue';
 import Create from './views/Create.vue';
 
@@ -25,6 +26,11 @@ export default new Router({
       path: '/create',
       name: 'create',
       component: Create
+    },
+    {
+      path: '/Help',
+      name: 'Help',
+      component: Help
     },
   ]
 })
